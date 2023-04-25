d881d5b2c6558d2965f9dea575f22d0a399a9131
Mohamed6247 <125515484+Mohamed6247@users.noreply.github.com>
2023-04-25 19:14:32 +0000

    Update teamprintf.c

--- teamprintf.c
+++ teamprintf.c
@@ -18,7 +18,7 @@ int _printf(const char *format, ...)
 
 	va_start(list, format);
 
-	for (format[i] != '\0' && i = 0; format; i++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
